849bbc7d369ee65c343ec01d7d931563310a1542
Nurulazrad M <nurulazrad@gmail.com>
2020-02-08 18:11:16 +0800

    Getting price data api

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,10 +3,18 @@ class HomeController < ApplicationController
     require 'net/http'
     require 'json'
 
+    # Grab News
     url = 'https://min-api.cryptocompare.com/data/v2/news/?lang=EN'
     uri = URI(url)
     response = Net::HTTP.get uri
     @news = JSON.parse response
+
+    # Grab Price
+    price_url = 'https://min-api.cryptocompare.com/data/pricemultifull?fsyms=BTC,ETH,MCO,EVN,NEO&tsyms=USD'
+    price_uri = URI(price_url)
+    price_response = Net::HTTP.get price_uri
+    @prices = JSON.parse price_response
+
   end
 
   def prices
